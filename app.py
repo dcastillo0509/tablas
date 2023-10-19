792ad5eed182757a8d2b855006bf1270e2a04a92
dcastillo0509 <143881222+dcastillo0509@users.noreply.github.com>
2023-10-18 20:39:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.graph_objs as go
 from backend.granulometria import granulometria
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
+server = app.server
 def generate_granulometria_plot():
     trace = go.Scatter(
         x=granulometria['Abertura'][0:11],
